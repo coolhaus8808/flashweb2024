f8bf4193d8fe49d10a46654c21879d1019c71439
Dániel Péter Baranyi <G8DIUJ@student.uni-pannon.hu>
2024-05-01 22:17:02 +0200

    admin.html

--- flashweb2024/webapp/routes.py
+++ flashweb2024/webapp/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, request, redirect, url_for, flash, session, j
 from webapp import app, db
 from werkzeug.security import generate_password_hash, check_password_hash
 from webapp.models import Events, User, MyCourse, Course, Degree, ApprovedDegree
-
+   
 def authenticate(username, password):
     user = User.query.filter_by(username=username).first()
     if user and check_password_hash(user.password_hash, password):
@@ -58,7 +58,15 @@ def login():
     username = request.form['username']
     password = request.form['password']
     user = authenticate(username, password)
-    if user:
+    if user and username == "admin":
+        
+        session['username'] = username
+        session['userid'] = user.id
+        return redirect(url_for('admin', user_id=user.id))
+        
+        
+        
+    elif user:
         
         session['username'] = username
         session['userid'] = user.id
@@ -78,10 +86,11 @@ def user(user_id):
         user = User.query.get_or_404(user_id)
         mycourses = MyCourse.query.filter_by(user_id=user_id).all()
         courses = Course.query.all()
-    
+        events = Events.query.all()
+            
 
         degrees = ApprovedDegree.query.distinct(ApprovedDegree.degree_id).all()
-        return render_template('user.html',user=user.username, user_id=user_id, mycourses=mycourses, courses=courses, degrees=degrees)
+        return render_template('user.html',user=user.username, user_id=user_id, mycourses=mycourses, courses=courses, degrees=degrees, events=events)
     else:
         flash('You are not logged in!', 'error')
         return redirect(url_for('index'))
@@ -215,3 +224,109 @@ def logout():
     session.clear()
     # Átirányítás az index oldalra
     return redirect(url_for("index"))
+
+@app.route("/admin/<int:user_id>")
+def admin(user_id):
+    if len(session) >0:
+            courses = Course.query.all()    
+            students = User.query.all()    
+            degrees = Degree.query.all()
+            events = Events.query.all()
+            approved = ApprovedDegree.query.all()
+    
+            user = User.query.get_or_404(user_id)
+            return render_template('admin.html',user=user.username, user_id=user_id, students = students, courses=courses, degrees=degrees, events=events,approved=approved)
+    else:
+        flash('You are not logged in!', 'error')
+        return redirect(url_for('index'))
+ 
+   
+
+@app.route("/edit_user/<int:user_id>", methods=['GET', 'POST'])
+def edit_user(user_id):
+    if request.method == 'POST':
+        if 'edit_selected_users' in request.form:
+            selected_users = request.form.getlist('selected_users[]')
+            for user_id in selected_users:
+                user = User.query.get_or_404(int(user_id))
+                username = request.form['username_' + user_id]
+                name = request.form['name_' + user_id]
+                degree_id = request.form['degree_' + user_id]
+                
+                user.username = username
+                user.name = name
+                user.degree_id = degree_id
+                
+                db.session.commit()
+            flash('Adatok frissulve!', 'success')
+    
+    users = User.query.all()
+    return render_template('admin_user_edit.html', users=users, user_id=user_id)
+
+@app.route("/edit_course/<int:user_id>", methods=['GET', 'POST'])
+def edit_course(user_id):
+    if request.method == 'POST':
+        if 'edit_selected_courses' in request.form:
+            selected_courses = request.form.getlist('selected_courses[]')
+            for course_id in selected_courses:
+                course = Course.query.get_or_404(int(course_id))
+                code = request.form['code_' + course_id]
+                name = request.form['name_' + course_id]
+                credit = request.form['credit_' + course_id]
+                
+                course.code = code
+                course.name = name
+                course.credit = credit
+                
+                db.session.commit()
+            flash('Adatok frissulve!', 'success')
+        elif 'add_new_course' in request.form:
+            new_code = request.form['new_code']
+            new_name = request.form['new_name']
+            new_credit = request.form['new_credit']
+
+            # Ellenõrzés, hogy az adatok megfelelõek-e, és hozzáadás az adatbázishoz
+            if new_code and new_name and new_credit:
+                new_course = Course(code=new_code, name=new_name, credit=new_credit)
+                db.session.add(new_course)
+                db.session.commit()
+
+                flash('Kurzus succes', 'success')
+            else:
+                flash('Minden adat kell!', 'error')
+    
+    courses = Course.query.all()
+    return render_template('admin_course_edit.html', courses=courses, user_id=user_id)
+
+@app.route("/edit_approved_degrees/<int:user_id>", methods=['GET', 'POST'])
+def edit_approved_degrees(user_id):
+    if request.method == 'POST':
+        if 'edit_selected_degrees' in request.form:
+            selected_degrees = request.form.getlist('selected_degrees')
+
+            for degree_id in selected_degrees:
+                degree = ApprovedDegree.query.get_or_404(degree_id)
+                new_course_id = request.form[f'course_id_{degree_id}']
+                new_degree_id = request.form[f'degree_id_{degree_id}']
+
+                if new_course_id and new_degree_id:
+                    degree.course_id = new_course_id
+                    degree.degree_id = new_degree_id
+                    db.session.commit()
+                else:
+                    flash('Minden adat kotelezo!', 'error')
+
+        elif 'add_new_degree' in request.form:
+            new_course_id = request.form['new_course_id']
+            new_degree_id = request.form['new_degree_id']
+
+            if new_course_id and new_degree_id:
+                new_degree = ApprovedDegree(course_id=new_course_id, degree_id=new_degree_id)
+                db.session.add(new_degree)
+                db.session.commit()
+                flash('Uj sor hozzaadva!', 'success')
+            else:
+                flash('Minden adat kotelezo!', 'error')
+
+    approved_degrees = ApprovedDegree.query.all()
+    return render_template('admin_approved.html', approved_degrees=approved_degrees, user_id=user_id)
